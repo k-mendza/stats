1990547031ff21f5b62b42f19edc0b1286b56859
Karol Mendza <karolmendza@gmail.com>
2019-12-23 15:35:25 +0100

    Code refactor

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 VALUES_ARRAY = np.array([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6])
-BINS = [0, 1, 2, 3, 4, 5, 6, 7] # bins are segments for grouping the chart
+BINS = [0, 1, 2, 3, 4, 5, 6, 7]  # bins are segments for grouping the chart
 HISTOGRAM_TITLE = 'Histogram'
 X_AXIS_LABEL = 'Wartość'
 Y_AXIS_LABEL = 'Liczba wystąpień'
 Y_AXIS_TICKS = [0, 1, 2, 3]
 Y_AXIS_LIMIT = 3
 
-plt.hist(VALUES_ARRAY, BINS, histtype='bar', rwidth=0.8)
+plt.hist(VALUES_ARRAY, BINS, histtype='bar', rwidth=0.8, color='blue')
 plt.title(HISTOGRAM_TITLE)
 plt.grid(axis='y', alpha=0.75)
 plt.xlabel(X_AXIS_LABEL)
