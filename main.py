10469ad2ad8e1512a3077d7abd48b03c192c1873
Karol Mendza <karolmendza@gmail.com>
2019-12-23 16:26:18 +0100

    Return to auto scale of bins and y axis

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ import csv
 
 _VALUE_CSV_COL_POSITION_ = 1
 _FILE_NAME_ = 'data.csv'
+_HISTOGRAM_TITLE_ = 'Histogram'
+_X_AXIS_LABEL_ = 'Wartość'
+_Y_AXIS_LABEL_ = 'Liczba wystąpień'
 
-VALUES_ARRAY = []
 
-with open(_FILE_NAME_, 'r') as csv_file:
-    data = csv.reader(csv_file)
-    for row in data:
-        VALUES_ARRAY.append(row[_VALUE_CSV_COL_POSITION_])
+def getValueArrayFromCSVFile(file_name):
+    file_data = []
+    with open(file_name, 'r') as csv_file:
+        data = csv.reader(csv_file)
+        for row in data:
+            file_data.append(int(row[_VALUE_CSV_COL_POSITION_]))
+    return file_data
 
-BINS = [0, 1, 2, 3, 4, 5, 6, 7]  # bins are segments for grouping the chart
-HISTOGRAM_TITLE = 'Histogram'
-X_AXIS_LABEL = 'Wartość'
-Y_AXIS_LABEL = 'Liczba wystąpień'
-Y_AXIS_TICKS = [0, 1, 2, 3]
-Y_AXIS_LIMIT = 3
 
-plt.hist(VALUES_ARRAY, BINS, histtype='bar', rwidth=0.8, color='blue')
-plt.title(HISTOGRAM_TITLE)
+values_array = np.array(getValueArrayFromCSVFile(_FILE_NAME_))
+
+plt.hist(values_array, bins='auto', histtype='bar', rwidth=0.8, color='blue')
+plt.title(_HISTOGRAM_TITLE_)
 plt.grid(axis='y', alpha=0.75)
-plt.xlabel(X_AXIS_LABEL)
-plt.ylabel(Y_AXIS_LABEL)
-plt.yticks(Y_AXIS_TICKS)
-plt.ylim(ymax=Y_AXIS_LIMIT)
+plt.xlabel(_X_AXIS_LABEL_)
+plt.ylabel(_Y_AXIS_LABEL_)
 
 plt.show()
